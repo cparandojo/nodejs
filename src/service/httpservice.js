4f107188d2634aecb057cbc92d89f62011bc8d2b
cparandojo <cparandojo@indra.es>
2018-02-06 15:40:42 +0000

    Modularización http

--- src/service/httpservice.js
+++ src/service/httpservice.js
@@ -8,7 +8,7 @@ exports.getHttpServices = function() {
 
     var router = express.Router();
     
-    var userdao = require('../dao/userdao.js');
+   
 
     //declaramos las rutas donde escucharemos para obtener el listado de usuarios.
     router.get('/users', function(req, res) {
@@ -16,21 +16,21 @@ exports.getHttpServices = function() {
         var users = [];
       
         //TODO obtener de mongo los datos.
-        //TODO recorrer la lista que sale de mongo y añadirlo al obtejo users para la respuesta.
+       
 
-        var user = {
-          "name": "name",
-          "email": "aaa@bb.com",
-          "username" : "username",
-          "phone" : "123456789"
-        }
+                var user = {
+                "name": "name",
+                "email": "aaa@bb.com",
+                "username" : "username",
+                "phone" : "123456789"
+                }
       
+         //TODO recorrer la lista que sale de mongo y añadirlo al obtejo users para la respuesta.
+
         users.push(user);
       
         res.setHeader('Content-Type', 'application/json');
-        res.send(JSON.stringify(users));
-      
-        console.log('Respondemos con el listado.' , JSON.stringify(users));
+        res.status(200).jsonp(users);
 
       });
 
@@ -41,20 +41,17 @@ exports.getHttpServices = function() {
         //obtenemos el body de la petición
         var requestBody = req.body;
 
-        console.log('El body de la petición es: ', JSON.stringify(requestBody));
 
-        
-        //TODO guardar en mongo
+        //TODO guardar en mongo 
 
         var responseOK={
-            code : 200,
             message : "Succes",
             data: requestBody
         }
 
 
         res.setHeader('Content-Type', 'application/json');
-        res.send(JSON.stringify(responseOK));
+        res.status(201).jsonp(responseOK);
         
       });
 
