9aa1aa875e58e28c1c33eee6a4e4805efb7d94bb
carlos <carlosperezarandojo@gmail.com>
2018-02-06 22:54:07 +0100

    Intento conexión con bbdd para crear usuario

--- src/model/user.js
+++ src/model/user.js
@@ -1,13 +1,15 @@
 
-var mongoose = require('mongoose');
-var Schema   = mongoose.Schema;
 
-//declaramos el tipo de dato user.
-var userSchema = new Schema({
-  name:    { type: String },
-  username:     { type: String },
-  email:  { type: String },
-  phone:   { type: String }
-});
+  var mongoose = require('mongoose');
+  var Schema   = mongoose.Schema;
 
+  //declaramos el tipo de dato user.
+  var userSchema = new Schema({
+    name:    { type: String },
+    username:     { type: String },
+    email:  { type: String },
+    phone:   { type: String }
+  });
+
+  
 module.exports = mongoose.model('user', userSchema);
--- src/service/users/httpservice.js
+++ src/service/users/httpservice.js
@@ -2,61 +2,62 @@
 /**
  * Servicio que contiene las peticiones http necesarias.
  */
-exports.getHttpServices = function() {
+exports.getHttpServices = function () {
+
+    //importamos el modelo.
+    var userModel = require('../../model/user.js');
 
     var express = require("express");
-
     var router = express.Router();
-    
-   
 
-    //declaramos las rutas donde escucharemos para obtener el listado de usuarios.
-    router.get('/users', function(req, res) {
-    
-        var users = [];
-      
-        //TODO obtener de mongo los datos.
-       
-
-                var user = {
-                "name": "name",
-                "email": "aaa@bb.com",
-                "username" : "username",
-                "phone" : "123456789"
-                }
-      
-         //TODO recorrer la lista que sale de mongo y añadirlo al obtejo users para la respuesta.
-
-        users.push(user);
-      
-        res.setHeader('Content-Type', 'application/json');
-        res.status(200).jsonp(users);
-
-      });
-
-
-      //Declaramos la ruta para realizar un post de un usuario.
-      router.post('/user', function(req, res){
+    //Declaramos la ruta para realizar un post de un usuario.
+    router.post('/user', function (req, res) {
 
         //obtenemos el body de la petición
         var requestBody = req.body;
 
+        //importamos el dao de crear.
+        var createUserDao = require('../../dao/users/createuserdao.js');
 
-        //TODO guardar en mongo 
+        //realizamos la llamada al DAO
+        createUserDao.createUser(requestBody);
 
-        var responseOK={
-            message : "Succes",
+        var responseOK = {
+            message: "Succes",
             data: requestBody
         }
 
-
         res.setHeader('Content-Type', 'application/json');
         res.status(201).jsonp(responseOK);
-        
-      });
 
-      //retornamos el objeto router para que sea añadido en app
-      return router;
+    });
+
+    //declaramos las rutas donde escucharemos para obtener el listado de usuarios.
+    router.get('/users', function (req, res) {
+
+        var users = [];
+
+        //TODO obtener de mongo los datos.
+
+
+        var user = {
+            "name": "name",
+            "email": "aaa@bb.com",
+            "username": "username",
+            "phone": "123456789"
+        }
+
+        //TODO recorrer la lista que sale de mongo y añadirlo al obtejo users para la respuesta.
+
+        users.push(user);
+
+        res.setHeader('Content-Type', 'application/json');
+        res.status(200).jsonp(users);
+
+    });
+
+    //retornamos el objeto router para que sea añadido en app
+    return router;
 
 };
 
